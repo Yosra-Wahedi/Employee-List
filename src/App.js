bc8da5ad4a8e0224cb9076cedd8c2ffae439c648
Yosra-Wahedi <119968130+Yosra-Wahedi@users.noreply.github.com>
2023-10-02 16:47:47 +0430

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import TotalCards from "./components/TotalCards";
+import { Data } from "../src/components/Data.js";
+import { useState } from "react";
 
 function App() {
+  const [employeesList, setEmployeesList] = useState(Data);
+  const handleFilterByAge = () => {
+    const minAge = document.getElementById("minAge").value;
+    const maxAge = document.getElementById("maxAge").value;
+    const ages = Array.from(new Set(Data.map((data) => data.age)));
+
+    const targetAges = ages.filter((ages) => {
+      return ages >= minAge && ages <= maxAge;
+    });
+
+    const filteredEmployees = Data.filter((employee) =>
+      targetAges.includes(employee.age)
+    );
+
+    setEmployeesList(filteredEmployees);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="app">
+      <header>
+        <h1>Employee List</h1>
+        <div>
+          <label htmlFor="minAge">Min Age:</label>
+          <input type="number" id="minAge" min="18" max="100" />
+          <label htmlFor="maxAge">Max Age:</label>
+          <input type="number" id="maxAge" min="18" max="100" />
+          <button id="btn" onClick={handleFilterByAge}>
+            Filter
+          </button>
+        </div>
       </header>
+      <div className="employees-container">
+        <TotalCards Data={employeesList}></TotalCards>
+      </div>
     </div>
   );
 }
